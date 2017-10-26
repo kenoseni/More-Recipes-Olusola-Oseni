52458d2d5dcbb31361e78e40f1f8fa54e26d72e2
Olusola Oseni <kenolusola@gmail.com>
2017-10-26 15:26:55 +0100

    Add recipe controller and route added

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -16,6 +16,22 @@ export default {
                 res.status(404).json({message: "no recipes found"})
             }
         })       
+    }, 
+    create(req, res) {
+        return new Promise ((resolve, reject) => {
+            if (req.body) {
+                recipeStore.push(req.body)
+                resolve(recipeStore)
+            }
+            reject({message: "no recipe added"})
+
+        })
+        .then(recipe => res.status(200).send({message: "recipe added"}))
+        .catch(error => {
+            if (error.message === "no recipe added") {
+                res.status(404).json({message: "no recipes added"})
+            }
+        })
     }
     
 }
