23d13ff86692c6678efc61939f62ece55d6c4556
Olusola Oseni <kenolusola@gmail.com>
2017-11-02 21:15:08 +0100

    Add script commands

--- bin/www.js
+++ bin/www.js
@@ -1,10 +1,4 @@
 // This is the application entry 
-import http from 'http'
-import app from '../app' // The express app we just created
 
-// Set the app entry port
-const port = parseInt(process.env.PORT, 10) || 8000;
-app.set('port', port);
+// import app from '../app' // The express app we just created
 
-const server = http.createServer(app);
-server.listen(port);
