7a8ec5c3ac3b669efd3da0de662f9a778e71ea24
Olusola Oseni <kenolusola@gmail.com>
2017-11-03 05:23:55 +0100

    new templates

--- js/myFile.js
+++ js/myFile.js
@@ -1,3 +1,22 @@
-const openNav = () => document.getElementById("mySidenav").style.width = "300px";
+$("#alert-target").click( () => {
+    toastr["info"]("I was launched via jQuery!")
+});
 
-const closeNav = () => document.getElementById("mySidenav").style.width = "0";
+$( () => {
+    $(".sticky").sticky({
+        topSpacing: 90
+        , zIndex: 2
+        , stopper: "#YourStopperId"
+    });
+});
+
+
+
+
+// SideNav Initialization
+// Show sideNav
+$('.button-collapse').sideNav('show');
+// Hide sideNav
+$('.button-collapse').sideNav('hide');
+// gives the appearance that a button has been toggled
+$().button('toggle')
